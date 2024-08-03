2b0276dce6c3cb1e266bdc2816948b957b4ee024
Adam Rhys Heaton <adamslinuxemail@gmail.com>
2024-08-03 16:08:03 -0400

    Update app.js
    1. Consistency: Consistent indentation and spacing for better readability.
    2. Validation: Trimmed input values to remove extra spaces.
    3. Commenting: Added comments for clarity and better understanding.

--- dist/projects/bookDatabase/js/app.js
+++ dist/projects/bookDatabase/js/app.js
@@ -11,22 +11,18 @@ class Book {
 class UI {
     static displayBooks() {
         const books = Store.getBooks();
-
         books.forEach((book) => UI.addBookToList(book));
     }
 
     static addBookToList(book) {
         const list = document.querySelector('#book-list');
-
         const row = document.createElement('tr');
-
         row.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-      `;
-
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
         list.appendChild(row);
     }
 
@@ -43,7 +39,6 @@ class UI {
         const container = document.querySelector('.container');
         const form = document.querySelector('#book-form');
         container.insertBefore(div, form);
-
         // Vanish in 3 seconds
         setTimeout(() => document.querySelector('.alert').remove(), 3000);
     }
@@ -64,7 +59,6 @@ class Store {
         } else {
             books = JSON.parse(localStorage.getItem('books'));
         }
-
         return books;
     }
 
@@ -76,13 +70,11 @@ class Store {
 
     static removeBook(isbn) {
         const books = Store.getBooks();
-
         books.forEach((book, index) => {
             if (book.isbn === isbn) {
                 books.splice(index, 1);
             }
         });
-
         localStorage.setItem('books', JSON.stringify(books));
     }
 }
@@ -96,15 +88,15 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     e.preventDefault();
 
     // Get form values
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const isbn = document.querySelector('#isbn').value;
+    const title = document.querySelector('#title').value.trim();
+    const author = document.querySelector('#author').value.trim();
+    const isbn = document.querySelector('#isbn').value.trim();
 
     // Validate
     if (title === '' || author === '' || isbn === '') {
         UI.showAlert('Please fill in all fields', 'danger');
     } else {
-        // Instatiate book
+        // Instantiate book
         const book = new Book(title, author, isbn);
 
         // Add Book to UI
